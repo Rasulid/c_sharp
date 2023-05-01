fba3c11fc181d4391524978f2cc39b1447c1b4ea
RasulAbduvaitov <96064378+Rasulid@users.noreply.github.com>
2023-05-01 16:44:51 +0500

    switch case

---
+++ lesson5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lesson5 { 
+
+    class Project { 
+
+        static void Main() {
+            
+            short num = Convert.ToInt16(Console.ReadLine());
+
+            switch (num) {
+
+                case 5 :
+                    Console.WriteLine("Number is 5 ");
+                    break;
+                case 15 :
+                    Console.WriteLine("Number is 15 ");
+                    break;
+                case 20 :
+                    Console.WriteLine("Number is 20 ");
+                    break;
+                case 25 :
+                    Console.WriteLine("Number is 25 ");
+                    break;
+                
+                default:
+                    Console.WriteLine("number is unknown");
+                    break;
+
+            }
+        }
+    }
+}
