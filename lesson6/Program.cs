ce4dc69ce90616a7dc393532618d9902ddf2e2bd
RasulAbduvaitov <96064378+Rasulid@users.noreply.github.com>
2023-05-02 14:55:51 +0500

    Loop

---
+++ lesson6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace lesson6 {
+
+    class Project { 
+
+        static void Main() {
+
+            // for (byte i = 0; i < 10; i++) {
+            //     Console.WriteLine("Element {0}", i);
+            // }
+
+            // bool isHeppy = true;
+
+            // while (isHeppy){
+
+            //     string and = Console.ReadLine();
+            //     if (and == "and") {
+            //         isHeppy = false;
+            //     }
+            // }
+
+            byte num = 100;
+
+            do {  // Один раз отработает , Сначало работает потом проверяет 
+
+                Console.WriteLine("Element " + num);
+
+            }while (num < 3);
+
+
+        }
+
+    }
+
+
+}
