f0eb46a9c15bf5d795867f749490bdd27200a6cc
RasulAbduvaitov <96064378+Rasulid@users.noreply.github.com>
2023-05-03 15:59:59 +0500

    x

---
+++ lesson9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lesson7 {
+    class Project {
+        static void Main(){
+
+            // Print("hello world");
+            // string word = Console.ReadLine();
+            // Print(word);
+
+        //     int s = Sum(12,12);
+        //     Print(s.ToString());
+
+        // }
+
+        // public static void Print(string word){
+        //     System.Console.WriteLine(word);
+        // }
+
+        // public static int Sum(int x, int y){
+        //     return x+y;
+            
+            
+        }
+
+        public static int Sum(int x, int y){
+
+
+        }
+
+    }
+}
