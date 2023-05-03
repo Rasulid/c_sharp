e28b3e6fcbbd394a0b5a8e16b0e53239a6e2d70c
RasulAbduvaitov <96064378+Rasulid@users.noreply.github.com>
2023-05-03 14:13:27 +0500

    for each , dynamic arrays

--- lesson8/Program.cs
+++ lesson8/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson8 { 
+
+    class Program { 
+    // for each , dynamic arrays
+        static void Main() { 
+
+            short[,] num = {
+                {12,53,56},
+                {13,54,55},
+                {14,55,56}
+            };
+
+            foreach(short i in num){
+                // System.Console.WriteLine("El :"+ i);
+            }
+
+    // dynamic array
+
+        List<int> numbers = new List<int>(){
+            12,13,14
+        };
+        numbers.Add(15);
+        numbers.Add(16);
+        numbers.Add(17);
+
+        numbers.Remove(15);
+        numbers.Sort();
+        numbers.Reverse();
+
+
+        foreach(int i in numbers){
+            System.Console.WriteLine("El :"+ i);
+        }
+
+
+        }
+
+    }
+
+
+}
