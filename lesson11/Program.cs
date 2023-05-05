1383c40c600b1da1edb9ee65781e17e5ad830183
RasulAbduvaitov <96064378+Rasulid@users.noreply.github.com>
2023-05-05 14:40:56 +0500

    try-catch

---
+++ lesson11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lesson11{
+    class Project{
+    // try-catch
+        static void Main() {
+            try {
+                int num = Convert.ToInt32(Console.ReadLine());
+                System.Console.WriteLine(num);
+            }catch(FormatException){
+                System.Console.WriteLine("Failed to convert");
+            }catch(Exception){
+                System.Console.WriteLine("Failed to convert2");
+            }finally{
+                System.Console.WriteLine("it's soo fucking good");
+            }
+        }
+    }
+}
