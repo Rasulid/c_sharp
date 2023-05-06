bbb24e10e74e8ef35c16b5903f00203da99d5e84
RasulAbduvaitov <96064378+Rasulid@users.noreply.github.com>
2023-05-06 11:46:58 +0500

    Creating classs

---
+++ lesson12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lesson12 {
+
+    class Project {
+
+        static void Main() {
+
+            Robot r = new Robot();
+            r.name = "Robot";
+            r.weight = 123;
+            r.coordinates = new byte[] {0,0,1};
+
+            System.Console.WriteLine(r.weight);
+
+        }
+
+    }
+
+}
---
+++ lesson12/Robot.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace lesson12 {
+
+    class Robot{
+        // public, protected , private
+
+        public string name;
+        public int weight;
+
+        public byte[] coordinates;
+    }
+
+}
